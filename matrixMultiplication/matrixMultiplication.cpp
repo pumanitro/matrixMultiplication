8e6a7d927ee80259ad598d7e008827ef4c584885
Patryk Janik <pumanitro@gmail.com>
2016-05-21 20:06:00 +0200

    First part of reading from files.

--- matrixMultiplication/matrixMultiplication.cpp
+++ matrixMultiplication/matrixMultiplication.cpp
@@ -8,6 +8,34 @@
 
 int main()
 {
+	//Czytanie z pliku
+	//Deklaracja plików
+	FILE *firstMatrixFile, *secondMatrixFile, *outputFile;
+
+	//Zmienne do danych z plików {FM - FirstMatrix | SM - SecondMatrix}
+	int FMRowLenght, FMColumnLenght, SMRowLenght, SMColumnLenght;
+	char test[5];
+
+	//Otwarcie plików do odczytu 
+	if ((firstMatrixFile = fopen("firstMatrixFile.txt", "r")) == NULL) {
+		printf("Nie mogê otworzyæ pliku firstMatrixFile.txt!\n");
+		exit(1);
+	}
+	else if ((firstMatrixFile = fopen("secondMatrixFile.txt", "r")) == NULL) {
+		printf("Nie mogê otworzyæ pliku secondMatrixFile.txt!\n");
+		exit(1);
+	}
+
+	//Czytanie po ca³ych plikach
+	while (fscanf(firstMatrixFile, "%s", test)!=EOF)
+	{
+		printf("%s", test);
+	}
+
+	//fprintf(fp, "%s", tekst); /* zapisz nasz ³añcuch w pliku */
+	fclose(firstMatrixFile); /* zamknij plik */
+
+
 	printf("Pierwszy program w tym roku xD\n");
 	system("pause");
     return 0;
