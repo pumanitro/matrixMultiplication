732202f4318d3000c5f3bd1f887ed0437d01e744
Patryk Janik <pumanitro@gmail.com>
2016-05-21 18:27:27 +0200

    Created project. Lets start :)

---
+++ matrixMultiplication/matrixMultiplication.cpp
@@ -0,0 +1,15 @@
+// matrixMultiplication.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	printf("Pierwszy program w tym roku xD\n");
+	system("pause");
+    return 0;
+}
+
